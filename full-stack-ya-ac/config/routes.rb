e83e9e98c057e446665e963f593b43b631460c46
Aaron Criner <aaron.criner@me.com>
2023-03-29 15:44:08 -0700

    added show MVC

--- full-stack-ya-ac/config/routes.rb
+++ full-stack-ya-ac/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get 'blogs' => 'blog#index'
+  get 'blogs' => 'blog#index', as: 'blogs'
+  get 'blogs/:id' => 'blog#show', as: 'show'
 end
